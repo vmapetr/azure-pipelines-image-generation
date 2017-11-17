3a6bac55691eaff9b6c860f782fe6e6ef0ac2e77
Lokesh Gopu <logopu@microsoft.com>
2017-11-17 09:21:02 -0500

    Fixing ServiceFabric install script

--- images/Windows/Vs2017/scripts/Install-ServiceFabricSDK.ps1
+++ images/Windows/Vs2017/scripts/Install-ServiceFabricSDK.ps1
@@ -5,4 +5,12 @@
 ##         must be install after Visual Studio
 ################################################################################
 
+#Creating 'Installer' cache folder if it doesn't exist
+$temp_install_dir = 'C:\Windows\Installer'
+New-Item -Path $temp_install_dir -ItemType Directory -Force
+
+Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
+
 WebpiCmd.exe /Install /Products:MicrosoftAzure-ServiceFabric-CoreSDK /AcceptEula
+
+Get-Content $env:Temp\InstallFabricRuntime.log
