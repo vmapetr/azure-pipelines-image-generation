9cc6d51e84690caade523fd2fe796e6f5f34bb95
Niadak <niadak@microsoft.com>
2017-11-17 21:23:04 +0530

    Adding static validation for SqlPS installation

--- images/Windows/Vs2017/scripts/Install-SQLPowerShellTools.ps1
+++ images/Windows/Vs2017/scripts/Install-SQLPowerShellTools.ps1
@@ -52,4 +52,8 @@ $SharedManagementObjectsExitCode = InstallMSI -MsiUrl "https://download.microsof
 
 $PowerShellToolsExitCode = InstallMSI -MsiUrl "https://download.microsoft.com/download/8/7/2/872BCECA-C849-4B40-8EBE-21D48CDF1456/ENU/x64/PowerShellTools.msi" -MsiName "PowerShellTools.msi"
 
+$modules = Get-Module -Name SQLPS -ListAvailable
+Write-Host "The SQLPS Modules present are:"
+$modules | Select-Object Name,Version,Path | Format-Table
+
 exit $PowerShellToolsExitCode
