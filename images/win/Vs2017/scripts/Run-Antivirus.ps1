4f79363257b18fb460c681d13e4efc07cca5a3e5
Chris Patterson <chrispat@microsoft.com>
2018-01-10 19:34:51 +0000

    Disabling a few more antivirus features

--- images/win/Vs2017/scripts/Run-Antivirus.ps1
+++ images/win/Vs2017/scripts/Run-Antivirus.ps1
@@ -10,3 +10,18 @@ Push-Location "C:\Program Files\Windows Defender"
 # Full Scan
 .\MpCmdRun.exe -Scan -ScanType 2
 Pop-Location
+
+Write-Host "Disable Antivirus"
+Set-MpPreference -DisableRealtimeMonitoring $true `
+                -DisableBehaviorMonitoring $true `
+                -DisableIOAVProtection $true `
+                -DisableIntrusionPreventionSystem $true `
+                -DisableScriptScanning $true `
+                -DisableArchiveScanning $true `
+                -DisableCatchupFullScan $true `
+                -DisableCatchupQuickScan $true `
+                -DisableEmailScanning $true `
+                -DisableRemovableDriveScanning $true `
+                -DisableRestorePoint $true `
+                -ScanAvgCPULoadFactor 0 `
+                -ExclusionPath "D:\", "C:\"
