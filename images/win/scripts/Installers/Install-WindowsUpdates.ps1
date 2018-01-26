05264dcd3b69344fc54a82ab85c626283df16c1a
Lokesh Gopu <logopu@microsoft.com>
2018-01-26 14:53:42 -0500

    Updating Install-WindowsUpdates.ps1

--- images/win/scripts/Installers/Install-WindowsUpdates.ps1
+++ images/win/scripts/Installers/Install-WindowsUpdates.ps1
@@ -6,6 +6,6 @@
 ################################################################################
 
 Write-Host "Run windows updates"
-Install-Module -Name PSWindowsUpdate -Force -AllowClobber -confirm:$false
+Install-Module -Name PSWindowsUpdate -Force -AllowClobber
 Get-WUInstall -WindowsUpdate -AcceptAll -UpdateType Software -IgnoreReboot
 Get-WUInstall -MicrosoftUpdate -AcceptAll -IgnoreUserInput -IgnoreReboot
