28cab5d39ebcba88baaf9b4aa77a842c4b105fdf
Lokesh Gopu <logopu@microsoft.com>
2018-01-29 17:05:06 -0500

    Added Docker and VS2017 Markdown

--- images/win/scripts/Installers/Vs2017/Validate-Docker.ps1
+++ images/win/scripts/Installers/Vs2017/Validate-Docker.ps1
@@ -9,10 +9,36 @@ if((Get-Command -Name 'docker') -and (Get-Command -Name 'docker-compose'))
 {
     Write-Host "docker $(docker version) on path"
     Write-Host "docker-compose $(docker-compose version) on path"
-    exit 0
 }
 else
 {
      Write-Host "docker or docker-compose are not on path"
     exit 1
 }
+
+# Adding description of the software to Markdown
+$SoftwareName = "Docker"
+
+$version = $(docker version --format '{{.Server.Version}}')
+
+$Description = @"
+_Version:_ $version<br/>
+_Environment:_
+* PATH: $((Get-Command docker).Source)
+"@
+
+Add-SoftwareDetailsToMarkdown -SoftwareName $SoftwareName -DescriptionMarkdown $Description
+
+
+
+$SoftwareName = "Docker-compose"
+
+$version = $(docker-compose version) | %{ if( ($_.Split(' ')[0]).Trim() -like "docker-compose") { ($_.Split(' ')[2]).Split(',')[0] } }
+
+$Description = @"
+_Version:_ $version<br/>
+_Environment:_
+* PATH: $((Get-Command docker-compose).Source)
+"@
+
+Add-SoftwareDetailsToMarkdown -SoftwareName $SoftwareName -DescriptionMarkdown $Description
