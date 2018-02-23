89c0392c98be1d2e8dcdb5e53ceb2dd64d7a8f2c
Lokesh Gopu <logopu@microsoft.com>
2018-02-23 10:27:55 -0500

    Adding  --ignore-checksums option for Google chrome installation

--- images/win/scripts/Installers/Install-Chrome.ps1
+++ images/win/scripts/Installers/Install-Chrome.ps1
@@ -7,7 +7,7 @@
 $temp_install_dir = 'C:\Windows\Installer'
 New-Item -Path $temp_install_dir -ItemType Directory -Force
 
-choco install googlechrome
+choco install googlechrome --ignore-checksums
 
 New-Item -Path "HKLM:\SOFTWARE\Policies\Google\Update" -Force
 New-ItemProperty "HKLM:\SOFTWARE\Policies\Google\Update" -Name "AutoUpdateCheckPeriodMinutes" -Value 00000000 -Force
