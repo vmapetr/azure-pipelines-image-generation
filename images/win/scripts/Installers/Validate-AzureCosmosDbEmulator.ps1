b33bd74422b0acff9b0cc221bde307710bf1298b
Austen Dicken <cvpcsm@gmail.com>
2018-04-19 12:02:02 -0500

    Install Azure Cosmos DB Emulator on the VS2017 images

---
+++ images/win/scripts/Installers/Validate-AzureCosmosDbEmulator.ps1
@@ -0,0 +1,26 @@
+################################################################################
+##  File:  Validate-AzureCosmosDbEmulator.ps1
+##  Team:  Automated Testing
+##  Desc:  Validate Azure CosmosDb Emulator installation.
+################################################################################
+
+$SoftwareName = 'Azure CosmosDb Emulator'
+$regKey = 'HKCU:\SOFTWARE\Microsoft\CosmosDBEmulator'
+
+if(Test-Path $regKey)
+{
+	$fileVersion = (Get-Item (Join-Path (Get-ItemProperty $regKey).'InstallDir' 'CosmosDB.Emulator.exe')).VersionInfo.FileVersion
+	$Description = @"
+_version:_
+$fileVersion
+"@
+	
+	Add-SoftwareDetailsToMarkdown -SoftwareName $SoftwareName -DescriptionMarkdown $Description
+	
+	exit 0
+}
+else
+{
+	Write-Host "$SoftwareName is not installed."
+    exit 1
+}
