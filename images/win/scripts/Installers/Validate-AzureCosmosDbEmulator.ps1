0786324cffe9f07d9d8917b4a1ebf2b294ae420f
Austen Dicken <audi@microsoft.com>
2018-04-26 18:06:46 -0500

    Add verbose logging for CosmosDB emulator validation script

--- images/win/scripts/Installers/Validate-AzureCosmosDbEmulator.ps1
+++ images/win/scripts/Installers/Validate-AzureCosmosDbEmulator.ps1
@@ -7,21 +7,43 @@
 $SoftwareName = 'Azure CosmosDb Emulator'
 $regKey = 'HKCU:\SOFTWARE\Microsoft\CosmosDBEmulator'
 
-if(Test-Path $regKey)
+if (!(Test-Path $regKey))
 {
-    $installDir = (Get-ItemProperty $regKey).'InstallDir'
-    $fileVersion = (Get-Item (Join-Path $installDir 'CosmosDB.Emulator.exe')).VersionInfo.FileVersion
+    Write-Host "The $regKey registry key is not set"
+    exit 1
+}
+else
+{
+    Write-Host "The $regKey registry key is set"
+}
+
+$installDir = (Get-ItemProperty $regKey).'InstallDir'
+if ($installDir -eq $null)
+{
+    Write-Host "The $SoftwareName installation directory registry value is not set"
+    exit 1
+}
+else
+{
+    Write-Host "The $SoftwareName installation directory registry value is set to: $installDir"
+}
+
+$exeFilePath = Join-Path $installDir 'CosmosDB.Emulator.exe'
+if (!(Test-Path $exeFilePath))
+{
+    Write-Host "$SoftwareName is not installed"
+    exit 1
+}
+else
+{
+    $fileVersion = (Get-Item $exeFilePath).VersionInfo.FileVersion
+    Write-Host "$SoftwareName is successfully installed: $fileVersion"
+
     $Description = @"
 _Version:_ $fileVersion<br/>
 _Location:_ $installDir
 "@
 
     Add-SoftwareDetailsToMarkdown -SoftwareName $SoftwareName -DescriptionMarkdown $Description
+}
 
-    exit 0
-}
-else
-{
-    Write-Host "$SoftwareName is not installed."
-    exit 1
-}
