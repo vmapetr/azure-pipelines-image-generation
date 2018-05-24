86684c70d7d460cd212a62911a2e465438f107e8
Lokesh Gopu <logopu@microsoft.com>
2018-05-24 16:28:55 -0400

    Added temp fix to install SSDT

--- images/win/scripts/Installers/Vs2017/Install-SSDT.ps1
+++ images/win/scripts/Installers/Vs2017/Install-SSDT.ps1
@@ -10,7 +10,19 @@ Import-Module -Name ImageHelpers -Force
 $InstallerURI = 'https://download.microsoft.com/download/A/3/B/A3BB5BE5-E8EA-4F63-B1CD-2346B104575E/SSDT-Setup-ENU.exe'
 $InstallerName = 'SSDT-Setup-ENU.exe'
 $logFilePath = "$env:TEMP\ssdtlog.txt"
-$ArgumentList = ('/install', 'INSTALLALL', '/passive', '/norestart', "/log `"$logFilePath`"")
+$ArgumentList = ('/install', 'INSTALLRS', 'INSTALLIS', '/passive', '/norestart', "/log `"$logFilePath`"")
+
+$exitCode = Install-EXE -Url $InstallerURI -Name $InstallerName -ArgumentList $ArgumentList
+
+if($exitCode -ne 0 -and $exitCode -ne 3010)
+{
+    Write-Host "******** SSDT SETUP LOG START ********"
+    Write-Host $(Get-Content $logFilePath | Out-String)
+    Write-Host "******** SSDT SETUP LOG END ********"
+    exit $exitCode
+}
+
+$ArgumentList = ('/install', 'INSTALLAS', '/passive', '/norestart', "/log `"$logFilePath`"")
 
 $exitCode = Install-EXE -Url $InstallerURI -Name $InstallerName -ArgumentList $ArgumentList
 
