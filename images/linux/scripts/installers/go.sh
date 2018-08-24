cbe41a4e872cfd79c83a087f30e9ee0a5e88d01a
David Staheli <dastahel@microsoft.com>
2018-08-24 10:53:31 -0400

    Include Go 1.9, 1.10, 1.11 (#204)

--- images/linux/scripts/installers/go.sh
+++ images/linux/scripts/installers/go.sh
@@ -2,23 +2,34 @@
 ################################################################################
 ##  File:  go.sh
 ##  Team:  CI-Platform
-##  Desc:  Installs go and configures GOROOT
+##  Desc:  Installs go, configures GOROOT, and adds go to the path
 ################################################################################
 
 ## Source the helpers for use with the script
 source $HELPER_SCRIPTS/document.sh
 
-VERSION=1.10
+# This function installs Go using the specified arguments:
+#   $1=MajorVersion (1.11)
+#   $2=MajorAndMinorVersion (1.11.1)
+#   $3=IsDefaultVersion (true or false)
+function InstallGo () {
+    curl -sL https://dl.google.com/go/go$2.linux-amd64.tar.gz -o go$2.linux-amd64.tar.gz
+    mkdir -p /usr/local/go$1
+    tar -C /usr/local/go$1 -xzf go$2.linux-amd64.tar.gz --strip-components=1 go
+    rm go$2.linux-amd64.tar.gz
+    echo "GOROOT_${1//./_}_X64=/usr/local/go$1" | tee -a /etc/environment
+    DocumentInstalledItem "Go $1 ($(/usr/local/go$1/bin/go version))"
 
-# Install Go
-curl -sL https://dl.google.com/go/go$VERSION.linux-amd64.tar.gz -o go$VERSION.linux-amd64.tar.gz
-mkdir -p /usr/local/go$VERSION
-tar -C /usr/local/go$VERSION -xzf go$VERSION.linux-amd64.tar.gz --strip-components=1 go
-rm go$VERSION.linux-amd64.tar.gz
+    # If this version of Go is to be the default version,
+    # symlink it into the path and point GOROOT to it.
+    if [ $3 = true ]
+    then
+        ln -s /usr/local/go$1/bin/* /usr/bin/
+        echo "GOROOT=/usr/local/go$1" | tee -a /etc/environment
+    fi
+}
 
-echo "GOROOT=/usr/local/go$VERSION" | tee -a /etc/environment
-
-ln -s /usr/local/go$VERSION/bin/* /usr/bin/
-
-## Document what was added to the image
-DocumentInstalledItem "Go $VERSION ($(go version))"
+# Install Go versions
+InstallGo 1.9 1.9.7 false
+InstallGo 1.10 1.10.3 true
+InstallGo 1.11 1.11rc2 false
