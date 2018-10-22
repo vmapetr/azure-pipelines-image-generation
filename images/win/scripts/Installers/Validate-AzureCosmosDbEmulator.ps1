cd192b2dcaffa53704509e25b16f6b0fcfbe57bd
Lokesh Gopu <logopu@microsoft.com>
2018-10-22 11:45:58 -0400

    Revert "Update Validate-AzureCosmosDbEmulator.ps1"
    This reverts commit 5000295eaaa4ce4cec4ea48db4836801bae4f443.

--- images/win/scripts/Installers/Validate-AzureCosmosDbEmulator.ps1
+++ images/win/scripts/Installers/Validate-AzureCosmosDbEmulator.ps1
@@ -5,7 +5,28 @@
 ################################################################################
 
 $SoftwareName = 'Azure CosmosDb Emulator'
-$installDir = 'C:\Program Files\Azure Cosmos DB Emulator'
+$regKey = 'HKCU:\SOFTWARE\Microsoft\CosmosDBEmulator'
+
+if (!(Test-Path $regKey))
+{
+    Write-Host "The $regKey registry key is not set"
+    exit 1
+}
+else
+{
+    Write-Host "The $regKey registry key is set"
+}
+
+$installDir = (Get-ItemProperty $regKey).'InstallDir'
+if ($installDir -eq $null)
+{
+    Write-Host "The $SoftwareName installation directory registry value is not set"
+    exit 1
+}
+else
+{
+    Write-Host "The $SoftwareName installation directory registry value is set to: $installDir"
+}
 
 $exeFilePath = Join-Path $installDir 'CosmosDB.Emulator.exe'
 if (!(Test-Path $exeFilePath))
