c8bdf279aa9fe005d44b24c6ba60a1bd1c05fcac
Manuel Riezebosch <mriezebosch@gmail.com>
2018-11-13 14:47:48 +0100

    mingw: copy of mingw32-make.exe shim instead of hardlink to original executable (#356)
    * Chocolatey package no longer installs in tools directory
    * Generated shims already discoverable
    * Resolving shim and copy makes it future proof

--- images/win/scripts/Installers/Install-MinGW.ps1
+++ images/win/scripts/Installers/Install-MinGW.ps1
@@ -7,9 +7,8 @@
 Import-Module -Name ImageHelpers -Force
 
 choco install -y mingw
-$installDir = "C:\tools\mingw64\bin"
-Add-MachinePathItem $installDir
 
-# Hard link mingw32-make.exe to make.exe, which is a more discoverable name
+# Make a copy of mingw32-make.exe to make.exe, which is a more discoverable name
 # and so the same command line can be used on Windows as on macOS and Linux
-New-Item -Path $installDir -ItemType HardLink -Name "make.exe" -Value "$installDir\mingw32-make.exe"
+$path = where.exe mingw32-make.exe | Get-Item
+Copy-Item -Path $path -Destination (Join-Path $path.Directory 'make.exe')
