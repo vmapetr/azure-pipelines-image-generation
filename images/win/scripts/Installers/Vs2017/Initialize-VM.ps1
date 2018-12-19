78f04621f554cdfb04da748b69809230420a5ace
Nathan Becker <nathan.becker@sterlingts.com>
2018-12-19 10:31:51 -0800

    Added Defender flag to allow it to use 100% of the CPU during the scan, fixed comment typo.

--- images/win/scripts/Installers/Vs2017/Initialize-VM.ps1
+++ images/win/scripts/Installers/Vs2017/Initialize-VM.ps1
@@ -47,7 +47,7 @@ else {
     Write-Host "Windows Update key does not exist"
 }
 
-# Insatll Windows .NET Features
+# Install Windows .NET Features
 Install-WindowsFeature -Name NET-Framework-Features -IncludeAllSubFeature
 Install-WindowsFeature -Name NET-Framework-45-Features -IncludeAllSubFeature
 Install-WindowsFeature -Name BITS -IncludeAllSubFeature
