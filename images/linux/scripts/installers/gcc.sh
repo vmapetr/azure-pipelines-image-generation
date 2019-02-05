c5c80b750069c8038bd5b2bc90589df7acb057f0
Eugeniy Khryaschev <eughrv@gmail.com>
2019-02-05 13:05:34 +0500

    Moved the g ++ installation to a separate installation script

--- images/linux/scripts/installers/gcc.sh
+++ images/linux/scripts/installers/gcc.sh
@@ -1,29 +1,26 @@
 #!/bin/bash
 ################################################################################
-##  File:  vcpkg.sh
+##  File:  gcc.sh
 ##  Team:  CI-Platform
-##  Desc:  Installs vcpkg
+##  Desc:  Installs GNU C++
 ################################################################################
 
 # Source the helpers for use with the script
 source $HELPER_SCRIPTS/document.sh
 
-VCPKG_BIN=/usr/local/share/vcpkg
+# Install GNU C++ compiler
+add-apt-repository ppa:ubuntu-toolchain-r/test -y
+apt-get update -y
+apt-get install g++-7 -y
 
-# Install vcpkg
-git clone --depth=1 https://github.com/Microsoft/vcpkg $VCPKG_BIN
-chmod 0755 $VCPKG_BIN
-$VCPKG_BIN/bootstrap-vcpkg.sh
-$VCPKG_BIN/vcpkg integrate install
-ln -sf /usr/local/share/vcpkg/vcpkg /usr/local/bin
 
 # Run tests to determine that the software installed as expected
 echo "Testing to make sure that script performed as expected, and basic scenarios work"
-if ! command -v vcpkg; then
-    echo "vcpkg was not installed"
+if ! command -v g++-7; then
+    echo "GNU C++ was not installed"
     exit 1
 fi
 
 # Document what was added to the image
 echo "Lastly, documenting what we added to the metadata file"
-DocumentInstalledItem "Vcpkg $(vcpkg version | head -n 1 | cut -d ' ' -f 6)"
+DocumentInstalledItem "GNU C++ $(g++-7 --version | head -n 1 | cut -d ' ' -f 4)"
