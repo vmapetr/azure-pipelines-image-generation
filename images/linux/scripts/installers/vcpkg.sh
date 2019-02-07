5230c9f52f55e99bb5fac831b8c78c1d5e081660
Maksim Petrov <47208721+vmapetr@users.noreply.github.com>
2019-02-07 16:42:28 +0300

    C/C++: Add vcpkg 0.0.113 (or latest) (Linux, Windows) (#682)
    Vcpkg provisioning scripts added for VS2017 and Ubuntu1604 image (Fixes issue #242 ). Also, added g++7 provisioning script for Ubuntu1604 as requirement for vcpkg.

---
+++ images/linux/scripts/installers/vcpkg.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+################################################################################
+##  File:  vcpkg.sh
+##  Team:  CI-Platform
+##  Desc:  Installs vcpkg
+################################################################################
+
+# Source the helpers for use with the script
+source $HELPER_SCRIPTS/document.sh
+
+VCPKG_BIN=/usr/local/share/vcpkg
+
+# Install vcpkg
+git clone --depth=1 https://github.com/Microsoft/vcpkg $VCPKG_BIN
+chmod 0755 $VCPKG_BIN
+$VCPKG_BIN/bootstrap-vcpkg.sh
+$VCPKG_BIN/vcpkg integrate install
+ln -sf /usr/local/share/vcpkg/vcpkg /usr/local/bin
+
+# Run tests to determine that the software installed as expected
+echo "Testing to make sure that script performed as expected, and basic scenarios work"
+if ! command -v vcpkg; then
+    echo "vcpkg was not installed"
+    exit 1
+fi
+
+# Document what was added to the image
+echo "Lastly, documenting what we added to the metadata file"
+DocumentInstalledItem "Vcpkg $(vcpkg version | head -n 1 | cut -d ' ' -f 6)"
