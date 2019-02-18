331cec1dc16e5e19da8dff1f7a873a6ecf3fb503
al-cheb <47745270+al-cheb@users.noreply.github.com>
2019-02-18 15:49:02 +0300

    Update Install-Git.ps1

--- images/win/scripts/Installers/Install-Git.ps1
+++ images/win/scripts/Installers/Install-Git.ps1
@@ -8,7 +8,7 @@ Import-Module -Name ImageHelpers
 
 # Install the latest version of Git which is bundled with Git LFS.
 # See https://chocolatey.org/packages/git
-choco install git -y --package-parameters= "/GitAndUnixToolsOnPath /WindowsTerminal /NoShellIntegration"
+choco install git -y --package-parameters="/GitAndUnixToolsOnPath /WindowsTerminal /NoShellIntegration"
 
 Add-MachinePathItem "C:\Program Files\Git\mingw64\bin"
 Add-MachinePathItem "C:\Program Files\Git\usr\bin"
