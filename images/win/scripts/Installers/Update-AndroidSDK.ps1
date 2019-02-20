cff640aba4119ecfffc9c7a4f6220784ab2a4143
Aleksandr Chebotov <47745270+al-cheb@users.noreply.github.com>
2019-02-20 10:49:39 +0300

    Add files via upload

--- images/win/scripts/Installers/Update-AndroidSDK.ps1
+++ images/win/scripts/Installers/Update-AndroidSDK.ps1
@@ -38,12 +38,7 @@ Expand-Archive -Path .\android-sdk-licenses.zip -DestinationPath 'C:\Program Fil
 $sdk_root = "C:\Program Files (x86)\Android\android-sdk"
 
 # The NDK is installed by Visual Studio at this location:
-$ndk_root = "C:\Microsoft\AndroidNDK64"
-
-# For VS before 15.7.0
-if(-not (Test-Path $ndk_root)) {
-    $ndk_root = "C:\ProgramData\Microsoft\AndroidNDK64"
-}
+$ndk_root = "C:\Microsoft\AndroidNDK64\"
 
 if(Test-Path $ndk_root){
 
