5ae80bbcb41d617fd746e40291408bc22b334682
Gary Ewan Park <gep13@gep13.co.uk>
2019-02-24 07:42:10 -0800

    (doc) Corrected spelling of PowerShell (#678)

--- images/win/scripts/Installers/Install-PowershellCore.ps1
+++ images/win/scripts/Installers/Install-PowershellCore.ps1
@@ -1,7 +1,7 @@
 ################################################################################
 ##  File:  Install-PowershellCore.ps1
 ##  Team:  CI-Platform
-##  Desc:  Install Powershel Core
+##  Desc:  Install PowerShell Core
 ################################################################################
 
 Invoke-Expression "& { $(Invoke-RestMethod https://aka.ms/install-powershell.ps1) } -UseMSI -Quiet"
