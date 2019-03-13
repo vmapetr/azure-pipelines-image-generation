f523f54347f0c810af55b2b5b7c540e687fe9426
David Staheli <dastahel@microsoft.com>
2019-03-13 08:04:57 -0400

    Add Go 1.12 (#779)
    * Add Go 1.12.  Remove Go 1.9 - an archived version.
    
    * Tweak
    
    * Remove Go 1.9.7 (archived version)
    
    * Keep Go 1.9.7 on Ubuntu 16.04 and VS2017 images

--- images/linux/scripts/installers/go.sh
+++ images/linux/scripts/installers/go.sh
@@ -32,4 +32,5 @@ function InstallGo () {
 # Install Go versions
 InstallGo 1.9 1.9.7 false
 InstallGo 1.10 1.10.8 false
-InstallGo 1.11 1.11.5 true
+InstallGo 1.11 1.11.5 false
+InstallGo 1.12 1.12 true
