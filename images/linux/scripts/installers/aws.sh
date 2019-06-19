62768dd96ea491cafc8de7e3bf885aecde47046e
David Staheli <dastahel@microsoft.com>
2019-06-19 12:41:37 -0400

    Remove AWS installer directory when done

--- images/linux/scripts/installers/aws.sh
+++ images/linux/scripts/installers/aws.sh
@@ -11,8 +11,9 @@ source $HELPER_SCRIPTS/document.sh
 # Install the AWS CLI
 curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
 unzip awscli-bundle.zip
-rm awscli-bundle.zip
 ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
+rm awscli-bundle.zip
+rm -rf awscli-bundle
 
 # Validate the installation
 echo "Validate the installation"
