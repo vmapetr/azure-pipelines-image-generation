27696fa276771ea0bbd7879c2cf2bd94d7bceb55
Preeti Bansal <46021667+prebansa@users.noreply.github.com>
2019-07-05 10:28:04 +0530

    Changes in installer script to pick up latest helm version (#1055)

--- images/linux/scripts/installers/1604/kubernetes-tools.sh
+++ images/linux/scripts/installers/1604/kubernetes-tools.sh
@@ -18,11 +18,7 @@ apt-get update
 apt-get install -y kubectl
 
 # Install Helm
-curl -sL https://storage.googleapis.com/kubernetes-helm/helm-v2.14.0-linux-amd64.tar.gz -o helm-v2.14.0-linux-amd64.tar.gz
-tar -zxvf helm-v2.14.0-linux-amd64.tar.gz
-chmod +x linux-amd64/helm
-mv linux-amd64/helm /usr/local/bin/helm
-rm helm-v2.14.0-linux-amd64.tar.gz
+curl -L https://git.io/get_helm.sh | bash
 
 # Run tests to determine that the software installed as expected
 echo "Testing to make sure that script performed as expected, and basic scenarios work"
