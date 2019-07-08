42af9a8d614c2ff8d2beaa0f6d60d747ec83858e
Gaurav Saral <gsaral@microsoft.com>
2019-07-08 10:42:55 +0530

    set env at machine level as well

--- images/win/scripts/Installers/Install-AzureCli.ps1
+++ images/win/scripts/Installers/Install-AzureCli.ps1
@@ -9,4 +9,5 @@ choco install azure-cli -y
 $AzureCliExtensionPath = Join-Path $Env:CommonProgramFiles 'AzureCliExtensionDirectory'
 New-Item -ItemType "directory" -Path $AzureCliExtensionPath
 
+[Environment]::SetEnvironmentVariable("AZURE_EXTENSION_DIR", $AzureCliExtensionPath, "Machine")
 $Env:AZURE_EXTENSION_DIR = $AzureCliExtensionPath
