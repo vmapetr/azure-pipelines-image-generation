0250094ad77598fcd4f9c3358a456d7a035b63e2
Gaurav Saral <gsaral@microsoft.com>
2019-07-09 13:18:03 +0530

    Minor fix

--- images/win/scripts/Installers/Install-AzureCli.ps1
+++ images/win/scripts/Installers/Install-AzureCli.ps1
@@ -9,5 +9,5 @@ choco install azure-cli -y
 $AzureCliExtensionPath = Join-Path $Env:CommonProgramFiles 'AzureCliExtensionDirectory'
 New-Item -ItemType "directory" -Path $AzureCliExtensionPath
 
-[Environment]::SetEnvironmentVariable("AZURE_EXTENSION_DIR", $AzureCliExtensionPath, "Machine")
+[Environment]::SetEnvironmentVariable("AZURE_EXTENSION_DIR", $AzureCliExtensionPath, [System.EnvironmentVariableTarget]::Machine)
 $Env:AZURE_EXTENSION_DIR = $AzureCliExtensionPath
