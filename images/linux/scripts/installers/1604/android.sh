36e1e5ede5fc22467b5283947ce7d8cc9e1910b5
Kayla Ngan <kangan@microsoft.com>
2019-08-22 14:04:33 -0400

    Add Android build-tools 29

--- images/linux/scripts/installers/1604/android.sh
+++ images/linux/scripts/installers/1604/android.sh
@@ -40,6 +40,8 @@ echo "y" | ${ANDROID_ROOT}/tools/bin/sdkmanager --sdk_root=${ANDROID_SDK_ROOT} \
     "platforms;android-17" \
     "platforms;android-15" \
     "platforms;android-10" \
+    "build-tools:29.0.2" \
+    "build-tools:29.0.0" \
     "build-tools;28.0.3" \
     "build-tools;28.0.2" \
     "build-tools;28.0.1" \
@@ -109,6 +111,8 @@ DocumentInstalledItem "Android SDK Platform 17"
 DocumentInstalledItem "Android SDK Platform 15"
 DocumentInstalledItem "Android SDK Platform 10"
 DocumentInstalledItem "Android SDK Patch Applier v4"
+DocumentInstalledItem "Android SDK Build-Tools 29.0.2"
+DocumentInstalledItem "Android SDK Build-Tools 29.0.0"
 DocumentInstalledItem "Android SDK Build-Tools 28.0.3"
 DocumentInstalledItem "Android SDK Build-Tools 28.0.2"
 DocumentInstalledItem "Android SDK Build-Tools 28.0.1"
